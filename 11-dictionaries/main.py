49c44350276b660a056a4ec1658dead85ea8d7d6
Egormity <kotlaregor9@gmail.com>
2025-02-13 17:51:46 +0700

    dictionaries

---
+++ 11-dictionaries/main.py
@@ -0,0 +1,21 @@
+d = { "a": 1, "b": 2, "c": 3 }
+
+d.update({ "j": 10, "z": 99, 'asd': 11, "as": 12, "hasdasd": -1, "po": 5 })
+# print(d)
+
+# d.clear()
+# print(d)
+
+# print(len(d))
+
+# print(sorted(d, key=len))
+
+# del d["a"]
+
+# for i in d:
+#     print(i, d[i])
+
+# print(d.keys(), d.values())
+
+# d.clear()
+# print(d)
